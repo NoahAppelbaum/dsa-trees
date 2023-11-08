cb2c435e73694abdf53cb9a61ddf4231bbfa010b
Noah Appelbaum <noahappelbaum@gmail.com>
2023-11-08 13:02:35 -0600

    tree class methods

--- tree.js
+++ tree.js
@@ -11,21 +11,39 @@ class TreeNode {
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
+    let sum = this.val;
 
+    for (const child of this.children){
+      sum += child.val;
+    }
+
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
+    let count = (this.val % 2 === 0 ? 1 : 0);
 
+    for (const child of this.children) {
+      count += (child.val % 2 === 0 ? 1 : 0);
+    }
+
+    return count;
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
   numGreater(lowerBound){
+    let count = (this.val > lowerBound ? 1 : 0);
 
+    for (const child of this.children) {
+      count += (child.val > lowerBound ? 1 : 0);
+    }
+
+    return count;
   }
 }
 
